08df5a98f1b4ea278a9c6674ade7fcb724cc52c0
Chae Taylor <49047762+SuperslowJelly@users.noreply.github.com>
2022-04-20 17:59:23 +0100

    🎨 Finalised previously implemented fix for sidebar height.

--- js/main.js
+++ js/main.js
@@ -29,12 +29,10 @@
 	};
 
 	var fullHeight = function () {
-		//if (!isMobile.any()) {
+		$(".js-fullheight").css("height", $(window).height());
+		$(window).resize(function () {
 			$(".js-fullheight").css("height", $(window).height());
-			$(window).resize(function () {
-				$(".js-fullheight").css("height", $(window).height());
-			});
-		//}
+		});
 	};
 
 	var counter = function () {
